162a8735164611240d78a4b9e338f068b9f87703
joele.cossu <j.cossu@list-group.com>
2025-01-28 20:32:22 +0100

    added new landing page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,9 +4,11 @@ import { HomeComponent } from './components/home/home.component';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 import { ProfileComponent } from './components/profile/profile.component';
 import { SettingsComponent} from './components/settings/settings.component';
+import { LandingComponent } from './components/landing/landing.component';
 
 export const routes: Routes = [
-    { path: '', title: "Login", component: LoginComponent },
+    { path: '', title: "Welcome", component: LandingComponent },
+    { path: 'login', title: "Login", component: LoginComponent },
     { path: 'home', title: "Home", component: HomeComponent }, 
     { path: 'profile', title: "Profile", component: ProfileComponent }, 
     { path: 'settings', title: "Settings", component: SettingsComponent }, 
