13fa4d4381ba8fb2710644926e85a18ee3e7c8d0
joele.cossu <j.cossu@list-group.com>
2025-01-28 06:37:25 +0100

    ui changes and login attempt

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,8 +2,13 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { HomeComponent } from './components/home/home.component';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { SettingsComponent} from './components/settings/settings.component';
+
 export const routes: Routes = [
     { path: '', title: "Login", component: LoginComponent },
-    { path: 'Home', title: "Home", component: HomeComponent },   
+    { path: 'home', title: "Home", component: HomeComponent }, 
+    { path: 'profile', title: "Profile", component: ProfileComponent }, 
+    { path: 'settings', title: "Settings", component: SettingsComponent }, 
     { path: "**", component: PageNotFoundComponent },
 ];
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,11 +1,36 @@
-import { Component } from '@angular/core';
-
+import { Component, NgModule } from '@angular/core';
+interface User {
+  username: string;
+  password: string;
+}
 @Component({
   selector: 'app-login',
   imports: [],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
-export class LoginComponent {
 
+export class LoginComponent {
+  username: string = '';
+  password: string = '';
+  loginMessage: string = '';
+
+  // Mock database
+  mockDB: { users: User[] } = {
+    users: [
+      { username: 'user1', password: 'password1' },
+      { username: 'user2', password: 'password2' }
+    ]
+  };
+
+  onSubmit() {
+    const user = this.mockDB.users.find(user => user.username === this.username && user.password === this.password);
+
+    if (user) {
+      this.loginMessage = 'Login successful!';
+    } else {
+      this.loginMessage = 'Invalid credentials';
+    }
+  }
 }
+
