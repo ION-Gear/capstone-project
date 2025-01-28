1197e64e1f2b37392c3398c739877714f7f7eda7
joele.cossu <j.cossu@list-group.com>
2025-01-28 18:57:49 +0100

    stable version

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,36 +1,49 @@
-import { Component, NgModule } from '@angular/core';
-interface User {
-  username: string;
-  password: string;
-}
+import { Component, signal,Output,EventEmitter } from '@angular/core';
+import { Router } from '@angular/router';
+import { CommonModule, NgClass } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 
 export class LoginComponent {
+  constructor(private router: Router) {}
   username: string = '';
   password: string = '';
-  loginMessage: string = '';
+  errorMessage = signal("");
+  // output the user via the loggedIn function in an output
+  @Output() loggedIn = new EventEmitter<string>();
+  
+  // temporary login database
+  users = [
+    {acc_email: 'joelecossu@hotmail.com', acc_password: 'jc1234'},
+    {acc_email: 'serena@hotmail.com', acc_password: 'sr1234'},
+    {acc_email: 'joelecossu117@gmail.com', acc_password: 'jc117'},
+    {acc_email: 'Gearpro17@hotmail.com', acc_password: 'gp17'},
+  ];
 
-  // Mock database
-  mockDB: { users: User[] } = {
-    users: [
-      { username: 'user1', password: 'password1' },
-      { username: 'user2', password: 'password2' }
-    ]
-  };
-
-  onSubmit() {
-    const user = this.mockDB.users.find(user => user.username === this.username && user.password === this.password);
-
-    if (user) {
-      this.loginMessage = 'Login successful!';
-    } else {
-      this.loginMessage = 'Invalid credentials';
+  showErrorMessage(){
+    this.errorMessage = signal("Invalid email or password");
+  }
+  handleLogin() {
+    // goes through the array and compares the inputs with the object in the index, 'u' is the object
+    const user = this.users.find(
+      (u) => u.acc_email === this.username && u.acc_password === this.password
+    );
+    if(user){
+      console.log("Login Succesful");
+      this.loggedIn.emit(this.username);
+      this.router.navigate(['/home']);
+    }
+    else {
+      console.log("Invalid email or password");
+      this.showErrorMessage();
     }
   }
+
 }
 
